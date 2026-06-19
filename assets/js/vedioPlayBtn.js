59fe396564ef7e6b8d9e5a67a468d96ebc6c1c38
dipesh <dipeshpohanekar1997@gmail.com>
2026-06-20 02:44:03 +0530

    Refactor video play button functionality: simplify play state toggle and update icon text

--- assets/js/vedioPlayBtn.js
+++ assets/js/vedioPlayBtn.js
@@ -1,10 +1,12 @@
 const videoPlayBtn = document.getElementById('video-play-btn');
+const playIcon = document.querySelector('#video-play-btn span');
 let playbtnflag = false;
 
 videoPlayBtn.addEventListener('click', () => {
     const videoOverlay = document.getElementById('video-overlay');
 
+    playbtnflag = !playbtnflag;
     videoOverlay.style.display = playbtnflag ? 'none' : 'block';
-    playbtnflag = playbtnflag ? false : true;
+    playIcon.textContent = playbtnflag ? 'pause' : 'play_arrow';
 });
 
